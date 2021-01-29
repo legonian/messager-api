d1e0b290387929bfe7527e4c7d772dbecddc40a9
Oleh <oleg.2014@gmail.com>
2021-01-29 15:03:57 +0200

    add appropriate http codes

--- message-controller.js
+++ message-controller.js
@@ -7,6 +7,7 @@ module.exports.messagePage = async function (req, res) {
   if (Number.isNaN(page)) {
     res.redirect('/api/messages/list/0')
   } else {
+    res.status(200)
     res.json(await Message.paginating(page, limit))
   }
 }
@@ -17,6 +18,7 @@ module.exports.newMessage = async function (req, res) {
   try {
     const mess = new Message(req.body)
     resJson.message = await mess.save()
+    res.status(201)
   } catch (err) {
     if (err.name === 'ValidationError') {
       console.log('ValidationError. POST body:', req.body)
@@ -24,6 +26,7 @@ module.exports.newMessage = async function (req, res) {
       console.log(err)
     }
     resJson.error = err.name
+    res.status(400)
   } finally {
     res.json(resJson)
   }
@@ -35,8 +38,10 @@ module.exports.oneMessage = async function (req, res) {
   try {
     const mess = await Message.findById(messageId).exec()
     mess.error = null
+    res.status(200)
     res.json(mess)
   } catch (err) {
+    res.status(404)
     res.json({ error: err.name })
   }
 }
